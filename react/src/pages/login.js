f55309d217b88cc2de1653fcb615409d7cce70b5
Benoit Arguel <benoit.arguel@gmail.com>
2019-05-21 13:52:22 +0200

    task: update props content

--- react/src/pages/login.js
+++ react/src/pages/login.js
@@ -1,102 +1,11 @@
 import React from 'react';
-import FormLogin from 'Components/formLogin';
-import FormSubscribe from 'Components/formSubscribe';
-import 'Styles/login.less';
-import { destroyJwtCookie } from 'Utils/jwtCookie';
-import PropTypes from 'prop-types';
+import MainLayout from 'Components/mainLayout';
+import Login from 'Components/login';
 
-class Login extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      mode: 'login',
-      animationStyle: {
-        transitionDuration: '300ms',
-        transform: 'translateY(0px)',
-        opacity: 1
-      }
-    };
-  }
-  componentDidMount() {
-    this.init();
-    destroyJwtCookie();
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.path !== prevProps.path) {
-      destroyJwtCookie();
-    }
-  }
-  init() {
-    this.setState({
-      animationStyle: {
-        transitionDuration: '300ms',
-        transform: 'translateY(0px)',
-        opacity: 1
-      }
-    });
-  }
-  switchMode(event) {
-    let {
-      mode
-    } = this.state;
-    event.preventDefault();
-    // sortie du formulaire
-    this.setState({
-      animationStyle: {
-        transitionDuration: '300ms',
-        transform: 'translateY(20px)',
-        opacity: 0
-      }
-    });
-    // repositionnement du formulaire avant arrivé
-    setTimeout(() =>
-      this.setState({
-        mode: mode === 'login' ? 'subscribe' : 'login',
-        animationStyle: {
-          transitionDuration: '0ms',
-          transform: 'translateY(-20px)',
-          opacity: 0
-        }
-      })
-    ,300);
-    // repositionnement du formulaire
-    setTimeout(
-      () => this.setState({
-        animationStyle: {
-          transitionDuration: '300ms',
-          transform: 'translateY(0px)',
-          opacity: 1
-        }
-      })
-    ,350);
-  }
-  render () {
-    const { mode, animationStyle } = this.state;
-    let propsContainerForm = {
-      className: `container-form ${mode}`,
-      style: animationStyle
-    };
-    return <div
-      ref="container"
-      className="container-page"
-    >
-      <div className="container-container-form">
-        <div {...propsContainerForm}>
-          {mode === 'login' ?
-            <FormLogin /> :
-              <FormSubscribe />
-            }
-            <a href="" onClick={e => this.switchMode(e)}>
-              {'Switch to ' + ( mode === 'login' ? 'subscribe' : 'login')}
-            </a>
-        </div>
-      </div>
+export default class extends React.Component {
+  render() {
+    return <div>
+      <MainLayout content={<Login />} />
     </div>;
   }
 }
-
-Login.propTypes = {
-  path: PropTypes.string
-};
-
-export default Login;
--- react/src/pages/read.js
+++ react/src/pages/read.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import SideBar from 'Components/sideBar';
-import MainContainer from 'Components/mainContainer';
-import Header from 'Components/header';
+import MainLayout from 'Components/mainLayout';
 
 export default class extends React.Component {
   render() {
     return <div>
-      <Header />
-      <SideBar />
-      <MainContainer />
+      <MainLayout />
     </div>;
   }
 }
